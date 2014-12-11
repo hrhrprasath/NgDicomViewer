2dc351cd8778ee689457480b8fcf5b41d955ed1b
Hari hara prasad Viswanathan <hr.hr.prasath@gmail.com>
2014-12-11 20:20:17 +0530

    Added demo
    Added demo

---
+++ demo/Src/Dwv/image/luts.js
@@ -0,0 +1,348 @@
+/** 
+ * Image module.
+ * @module image
+ */
+var dwv = dwv || {};
+dwv.image = dwv.image || {};
+dwv.image.lut = dwv.image.lut || {};
+
+/**
+ * Rescale LUT class.
+ * @class Rescale
+ * @namespace dwv.image.lut
+ * @constructor
+ * @param {Number} slope_ The rescale slope.
+ * @param {Number} intercept_ The rescale intercept.
+ */
+dwv.image.lut.Rescale = function(slope_,intercept_)
+{
+    /**
+     * The internal array.
+     * @property rescaleLut_
+     * @private
+     * @type Array
+     */
+    var rescaleLut_ = null;
+    
+    // Check the rescale slope.
+    if(typeof(slope_) === 'undefined') {
+        slope_ = 1;
+    }
+    // Check the rescale intercept.
+    if(typeof(intercept_) === 'undefined') {
+        intercept_ = 0;
+    }
+    
+    /**
+     * Get the rescale slope.
+     * @method getSlope
+     * @return {Number} The rescale slope.
+     */ 
+    this.getSlope = function() { return slope_; };
+    /**
+     * Get the rescale intercept.
+     * @method getIntercept
+     * @return {Number} The rescale intercept.
+     */ 
+    this.getIntercept = function() { return intercept_; };
+    
+    /**
+     * Initialise the LUT.
+     * @method initialise
+     * @param {Number} bitsStored The number of bits used to store the data.
+     */ 
+    // Initialise the LUT.
+    this.initialise = function(bitsStored)
+    {
+        var size = Math.pow(2, bitsStored);
+        rescaleLut_ = new Float32Array(size);
+        for(var i=0; i<size; ++i) {
+            rescaleLut_[i] = i * slope_ + intercept_;
+        }
+    };
+    
+    /**
+     * Get the length of the LUT array.
+     * @method getLength
+     * @return {Number} The length of the LUT array.
+     */ 
+    this.getLength = function() { return rescaleLut_.length; };
+    
+    /**
+     * Get the value of the LUT at the given offset.
+     * @method getValue
+     * @return {Number} The value of the LUT at the given offset.
+     */ 
+    this.getValue = function(offset) { return rescaleLut_[offset]; };
+};
+
+/**
+ * Window LUT class.
+ * @class Window
+ * @namespace dwv.image.lut
+ * @constructor
+ * @param {Number} rescaleLut_ The associated rescale LUT.
+ * @param {Boolean} isSigned_ Flag to know if the data is signed.
+ */
+dwv.image.lut.Window = function(rescaleLut_, isSigned_)
+{
+    /**
+     * The internal array: Uint8ClampedArray clamps between 0 and 255.
+     * (not supported on travis yet... using basic array, be sure not to overflow!)
+     * @property rescaleLut_
+     * @private
+     * @type Array
+     */
+    var windowLut_ = null;
+    
+    // check Uint8ClampedArray support
+    if( !dwv.browser.hasClampedArray() ) {
+        windowLut_ = new Uint8Array(rescaleLut_.getLength());
+    }
+    else {
+        windowLut_ = new Uint8ClampedArray(rescaleLut_.getLength());
+    }
+    
+    /**
+     * The window center.
+     * @property center_
+     * @private
+     * @type Number
+     */
+    var center_ = null;
+    /**
+     * The window width.
+     * @property width_
+     * @private
+     * @type Number
+     */
+    var width_ = null;
+    
+    /**
+     * Get the window center.
+     * @method getCenter
+     * @return {Number} The window center.
+     */ 
+    this.getCenter = function() { return center_; };
+    /**
+     * Get the window width.
+     * @method getWidth
+     * @return {Number} The window width.
+     */ 
+    this.getWidth = function() { return width_; };
+    /**
+     * Get the signed flag.
+     * @method isSigned
+     * @return {Boolean} The signed flag.
+     */ 
+    this.isSigned = function() { return isSigned_; };
+    
+    /**
+     * Set the window center and width.
+     * @method setCenterAndWidth
+     * @param {Number} center The window center.
+     * @param {Number} width The window width.
+     */ 
+    this.setCenterAndWidth = function(center, width)
+    {
+        // store the window values
+        center_ = center;
+        width_ = width;
+        // pre calculate loop values
+        var size = windowLut_.length;
+        var center0 = center - 0.5;
+        if ( isSigned_ ) {
+            center0 += rescaleLut_.getValue( parseInt(size / 2, 10) );
+        }
+        var width0 = width - 1;
+        var dispval = 0;
+        if( !dwv.browser.hasClampedArray() )
+        {
+            var yMax = 255;
+            var yMin = 0;
+            for(var j=0; j<size; ++j)
+            {
+                // from the DICOM specification (https://www.dabsoft.ch/dicom/3/C.11.2.1.2/)
+                // y = ((x - (c - 0.5)) / (w-1) + 0.5) * (ymax - ymin )+ ymin
+                dispval = ((rescaleLut_.getValue(j) - center0 ) / width0 + 0.5) * 255;
+                dispval = parseInt(dispval, 10);
+                if ( dispval <= yMin ) {
+                    windowLut_[j] = yMin;
+                }
+                else if ( dispval > yMax ) {
+                    windowLut_[j] = yMax;
+                }
+                else {
+                    windowLut_[j] = dispval;
+                }
+            }
+        }
+        else
+        {
+            // when using Uint8ClampedArray, values are clamped between 0 and 255
+            // no need to check
+            for(var i=0; i<size; ++i)
+            {
+                // from the DICOM specification (https://www.dabsoft.ch/dicom/3/C.11.2.1.2/)
+                // y = ((x - (c - 0.5)) / (w-1) + 0.5) * (ymax - ymin )+ ymin
+                dispval = ((rescaleLut_.getValue(i) - center0 ) / width0 + 0.5) * 255;
+                windowLut_[i]= parseInt(dispval, 10);
+            }
+        }
+    };
+    
+    /**
+     * Get the length of the LUT array.
+     * @method getLength
+     * @return {Number} The length of the LUT array.
+     */ 
+    this.getLength = function() { return windowLut_.length; };
+
+    /**
+     * Get the value of the LUT at the given offset.
+     * @method getValue
+     * @return {Number} The value of the LUT at the given offset.
+     */ 
+    this.getValue = function(offset)
+    {
+        var shift = isSigned_ ? windowLut_.length / 2 : 0;
+        return windowLut_[offset+shift];
+    };
+};
+
+/**
+* Lookup tables for image color display. 
+*/
+
+dwv.image.lut.range_max = 256;
+
+dwv.image.lut.buildLut = function(func)
+{
+    var lut = [];
+    for( var i=0; i<dwv.image.lut.range_max; ++i ) {
+        lut.push(func(i));
+    }
+    return lut;
+};
+
+dwv.image.lut.max = function(/*i*/)
+{
+    return dwv.image.lut.range_max-1;
+};
+
+dwv.image.lut.maxFirstThird = function(i)
+{
+    if( i < dwv.image.lut.range_max/3 ) {
+        return dwv.image.lut.range_max-1;
+    }
+    return 0;
+};
+
+dwv.image.lut.maxSecondThird = function(i)
+{
+    var third = dwv.image.lut.range_max/3;
+    if( i >= third && i < 2*third ) {
+        return dwv.image.lut.range_max-1;
+    }
+    return 0;
+};
+
+dwv.image.lut.maxThirdThird = function(i)
+{
+    if( i >= 2*dwv.image.lut.range_max/3 ) {
+        return dwv.image.lut.range_max-1;
+    }
+    return 0;
+};
+
+dwv.image.lut.toMaxFirstThird = function(i)
+{
+    var val = i * 3;
+    if( val > dwv.image.lut.range_max-1 ) {
+        return dwv.image.lut.range_max-1;
+    }
+    return val;
+};
+
+dwv.image.lut.toMaxSecondThird = function(i)
+{
+    var third = dwv.image.lut.range_max/3;
+    var val = 0;
+    if( i >= third ) {
+        val = (i-third) * 3;
+        if( val > dwv.image.lut.range_max-1 ) {
+            return dwv.image.lut.range_max-1;
+        }
+    }
+    return val;
+};
+
+dwv.image.lut.toMaxThirdThird = function(i)
+{
+    var third = dwv.image.lut.range_max/3;
+    var val = 0;
+    if( i >= 2*third ) {
+        val = (i-2*third) * 3;
+        if( val > dwv.image.lut.range_max-1 ) {
+            return dwv.image.lut.range_max-1;
+        }
+    }
+    return val;
+};
+
+dwv.image.lut.zero = function(/*i*/)
+{
+    return 0;
+};
+
+dwv.image.lut.id = function(i)
+{
+    return i;
+};
+
+dwv.image.lut.invId = function(i)
+{
+    return (dwv.image.lut.range_max-1)-i;
+};
+
+// plain
+dwv.image.lut.plain = {
+    "red":   dwv.image.lut.buildLut(dwv.image.lut.id),
+    "green": dwv.image.lut.buildLut(dwv.image.lut.id),
+    "blue":  dwv.image.lut.buildLut(dwv.image.lut.id)
+};
+
+// inverse plain
+dwv.image.lut.invPlain = {
+    "red":   dwv.image.lut.buildLut(dwv.image.lut.invId),
+    "green": dwv.image.lut.buildLut(dwv.image.lut.invId),
+    "blue":  dwv.image.lut.buildLut(dwv.image.lut.invId)
+};
+
+//rainbow 
+dwv.image.lut.rainbow = {
+    "blue":  [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 148, 152, 156, 160, 164, 168, 172, 176, 180, 184, 188, 192, 196, 200, 204, 208, 212, 216, 220, 224, 228, 232, 236, 240, 244, 248, 252, 255, 247, 239, 231, 223, 215, 207, 199, 191, 183, 175, 167, 159, 151, 143, 135, 127, 119, 111, 103, 95, 87, 79, 71, 63, 55, 47, 39, 31, 23, 15, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "green": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120, 128, 136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232, 240, 248, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 253, 251, 249, 247, 245, 243, 241, 239, 237, 235, 233, 231, 229, 227, 225, 223, 221, 219, 217, 215, 213, 211, 209, 207, 205, 203, 201, 199, 197, 195, 193, 192, 189, 186, 183, 180, 177, 174, 171, 168, 165, 162, 159, 156, 153, 150, 147, 144, 141, 138, 135, 132, 129, 126, 123, 120, 117, 114, 111, 108, 105, 102, 99, 96, 93, 90, 87, 84, 81, 78, 75, 72, 69, 66, 63, 60, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3],
+    "red":   [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 62, 60, 58, 56, 54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 148, 152, 156, 160, 164, 168, 172, 176, 180, 184, 188, 192, 196, 200, 204, 208, 212, 216, 220, 224, 228, 232, 236, 240, 244, 248, 252, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]
+};
+
+// hot
+dwv.image.lut.hot = {
+    "red":   dwv.image.lut.buildLut(dwv.image.lut.toMaxFirstThird),
+    "green": dwv.image.lut.buildLut(dwv.image.lut.toMaxSecondThird),
+    "blue":  dwv.image.lut.buildLut(dwv.image.lut.toMaxThirdThird)
+};
+
+// test
+dwv.image.lut.test = {
+    "red":   dwv.image.lut.buildLut(dwv.image.lut.id),
+    "green": dwv.image.lut.buildLut(dwv.image.lut.zero),
+    "blue":  dwv.image.lut.buildLut(dwv.image.lut.zero)
+};
+
+//red
+/*dwv.image.lut.red = {
+   "red":   dwv.image.lut.buildLut(dwv.image.lut.max),
+   "green": dwv.image.lut.buildLut(dwv.image.lut.id),
+   "blue":  dwv.image.lut.buildLut(dwv.image.lut.id)
+};*/
---
+++ demo/Src/Dwv/image/view.js
@@ -0,0 +1,423 @@
+/** 
+ * Image module.
+ * @module image
+ */
+var dwv = dwv || {};
+dwv.image = dwv.image || {};
+
+/**
+ * View class.
+ * @class View
+ * @namespace dwv.image
+ * @constructor
+ * @param {Image} image The associated image.
+ * @param {Boolean} isSigned Is the data signed.
+ * Need to set the window lookup table once created
+ * (either directly or with helper methods). 
+ */
+dwv.image.View = function(image, isSigned)
+{
+    /**
+     * Rescale lookup table.
+     * @property rescaleLut
+     * @private
+     * @type Rescale
+     */
+    var rescaleLut = new dwv.image.lut.Rescale(
+        image.getRescaleSlope(), image.getRescaleIntercept() );
+    // initialise it
+    rescaleLut.initialise(image.getMeta().BitsStored);
+    
+    /**
+     * Window lookup table.
+     * @property windowLut
+     * @private
+     * @type Window
+     */
+    var windowLut = new dwv.image.lut.Window(rescaleLut, isSigned);
+    
+    /**
+     * Window presets.
+     * @property windowPresets
+     * @private
+     * @type Object
+     */
+    var windowPresets = null;
+    /**
+     * Color map
+     * @property colorMap
+     * @private
+     * @type Object
+     */
+    var colorMap = dwv.image.lut.plain;
+    /**
+     * Current position
+     * @property currentPosition
+     * @private
+     * @type Object
+     */
+    var currentPosition = {"i":0,"j":0,"k":0};
+    
+    /**
+     * Get the associated image.
+     * @method getImage
+     * @return {Image} The associated image.
+     */ 
+    this.getImage = function() { return image; };
+    /**
+     * Set the associated image.
+     * @method setImage
+     * @param {Image} inImage The associated image.
+     */ 
+    this.setImage = function(inImage) { image = inImage; };
+    
+    /**
+     * Get the rescale LUT of the image.
+     * @method getRescaleLut
+     * @return {Rescale} The rescale LUT of the image.
+     */ 
+    this.getRescaleLut = function() { return rescaleLut; };
+    /**
+     * Set the rescale LUT of the image.
+     * @method setRescaleLut
+     * @param {Rescale} lut The rescale LUT of the image.
+     */ 
+    this.setRescaleLut = function(lut) { rescaleLut = lut; };
+
+    /**
+     * Get the window LUT of the image.
+     * @method getWindowLut
+     * @return {Window} The window LUT of the image.
+     */ 
+    this.getWindowLut = function() { return windowLut; };
+    /**
+     * Set the window LUT of the image.
+     * @method setWindowLut
+     * @param {Window} lut The window LUT of the image.
+     */ 
+    this.setWindowLut = function(lut) { windowLut = lut; };
+    
+    /**
+     * Get the window presets.
+     * @method getWindowPresets
+     * @return {Object} The window presets.
+     */ 
+    this.getWindowPresets = function() { return windowPresets; };
+    /**
+     * Set the window presets.
+     * @method setWindowPresets
+     * @param {Object} presets The window presets.
+     */ 
+    this.setWindowPresets = function(presets) { 
+        windowPresets = presets;
+        this.setWindowLevel(presets[0].center, presets[0].width);
+    };
+    
+    /**
+     * Get the color map of the image.
+     * @method getColorMap
+     * @return {Object} The color map of the image.
+     */ 
+    this.getColorMap = function() { return colorMap; };
+    /**
+     * Set the color map of the image.
+     * @method setColorMap
+     * @param {Object} map The color map of the image.
+     */ 
+    this.setColorMap = function(map) { 
+        colorMap = map;
+        // TODO Better handle this...
+        if( this.getImage().getPhotometricInterpretation() === "MONOCHROME1") {
+            colorMap = dwv.image.lut.invPlain;
+        }
+        this.fireEvent({"type": "colorchange", 
+           "wc": this.getWindowLut().getCenter(),
+           "ww": this.getWindowLut().getWidth() });
+    };
+    
+    /**
+     * Is the data signed data.
+     * @method isSigned
+     * @return {Boolean} The signed data flag.
+     */ 
+    this.isSigned = function() { return isSigned; };
+    
+    /**
+     * Get the current position.
+     * @method getCurrentPosition
+     * @return {Object} The current position.
+     */ 
+    this.getCurrentPosition = function() { return currentPosition; };
+    /**
+     * Set the current position. Returns false if not in bounds.
+     * @method setCurrentPosition
+     * @param {Object} pos The current position.
+     */ 
+    this.setCurrentPosition = function(pos) { 
+        if( !image.getSize().isInBounds(pos.i,pos.j,pos.k) ) {
+            return false;
+        }
+        var oldPosition = currentPosition;
+        currentPosition = pos;
+        // only display value for monochrome data
+        if( app.getImage().getPhotometricInterpretation().match(/MONOCHROME/) !== null )
+        {
+            this.fireEvent({"type": "positionchange", 
+                "i": pos.i, "j": pos.j, "k": pos.k,
+                "value": image.getRescaledValue(pos.i,pos.j,pos.k)});
+        }
+        else
+        {
+            this.fireEvent({"type": "positionchange", 
+                "i": pos.i, "j": pos.j, "k": pos.k});
+        }
+        // slice change event (used to trigger redraw)
+        if( oldPosition.k !== currentPosition.k ) {
+            this.fireEvent({"type": "slicechange"});
+        }
+        return true;
+    };
+    
+    /**
+     * View listeners
+     * @property listeners
+     * @private
+     * @type Object
+     */
+    var listeners = {};
+    /**
+     * Get the view listeners.
+     * @method getListeners
+     * @return {Object} The view listeners.
+     */ 
+    this.getListeners = function() { return listeners; };
+    /**
+     * Set the view listeners.
+     * @method setListeners
+     * @param {Object} list The view listeners.
+     */ 
+    this.setListeners = function(list) { listeners = list; };
+};
+
+/**
+ * Set the view window/level.
+ * @method setWindowLevel
+ * @param {Number} center The window center.
+ * @param {Number} width The window width.
+ * Warning: uses the latest set rescale LUT or the default linear one.
+ */
+dwv.image.View.prototype.setWindowLevel = function( center, width )
+{
+    this.getWindowLut().setCenterAndWidth(center, width);
+    this.fireEvent({"type": "wlchange", "wc": center, "ww": width });
+};
+
+/**
+ * Set the image window/level to cover the full data range.
+ * @method setWindowLevelMinMax
+ * Warning: uses the latest set rescale LUT or the default linear one.
+ */
+dwv.image.View.prototype.setWindowLevelMinMax = function()
+{
+    // calculate center and width
+    var range = this.getImage().getRescaledDataRange();
+    var min = range.min;
+    var max = range.max;
+    var width = max - min;
+    var center = min + width/2;
+    // set window level
+    this.setWindowLevel(center,width);
+};
+/**
+ * Go to first slice .
+ * @method goFirstSlice
+ * @return {Boolean} False if not in bounds.
+ */
+dwv.image.View.prototype.goFirstSlice = function()
+{
+    return this.setCurrentPosition({
+        "i": this.getCurrentPosition().i,
+        "j": this.getCurrentPosition().j,
+        "k":  0 
+    });
+};
+/**
+ * Increment the current slice number.
+ * @method incrementSliceNb
+ * @return {Boolean} False if not in bounds.
+ */
+dwv.image.View.prototype.incrementSliceNb = function()
+{
+    return this.setCurrentPosition({
+        "i": this.getCurrentPosition().i,
+        "j": this.getCurrentPosition().j,
+        "k": this.getCurrentPosition().k + 1 
+    });
+};
+
+/**
+ * Decrement the current slice number.
+ * @method decrementSliceNb
+ * @return {Boolean} False if not in bounds.
+ */
+dwv.image.View.prototype.decrementSliceNb = function()
+{
+    return this.setCurrentPosition({
+        "i": this.getCurrentPosition().i,
+        "j": this.getCurrentPosition().j,
+        "k": this.getCurrentPosition().k - 1 
+    });
+};
+
+/**
+ * Clone the image using all meta data and the original data buffer.
+ * @method clone
+ * @return {View} A full copy of this {dwv.image.Image}.
+ */
+dwv.image.View.prototype.clone = function()
+{
+    var copy = new dwv.image.View(this.getImage());
+    copy.setRescaleLut(this.getRescaleLut());
+    copy.setWindowLut(this.getWindowLut());
+    copy.setListeners(this.getListeners());
+    return copy;
+};
+
+/**
+ * Generate display image data to be given to a canvas.
+ * @method generateImageData
+ * @param {Array} array The array to fill in.
+ * @param {Number} sliceNumber The slice position.
+ */
+dwv.image.View.prototype.generateImageData = function( array )
+{        
+    var sliceNumber = this.getCurrentPosition().k;
+    var image = this.getImage();
+    var pxValue = 0;
+    var photoInterpretation = image.getPhotometricInterpretation();
+    var planarConfig = image.getPlanarConfiguration();
+    var windowLut = this.getWindowLut();
+    var colorMap = this.getColorMap();
+    var index = 0;
+    var sliceSize = 0;
+    var sliceOffset = 0;
+    switch (photoInterpretation)
+    {
+    case "MONOCHROME1":
+    case "MONOCHROME2":
+        sliceSize = image.getSize().getSliceSize();
+        sliceOffset = (sliceNumber || 0) * sliceSize;
+        var iMax = sliceOffset + sliceSize;
+        for(var i=sliceOffset; i < iMax; ++i)
+        {        
+            pxValue = parseInt( windowLut.getValue( 
+                    image.getValueAtOffset(i) ), 10 );
+            array.data[index] = colorMap.red[pxValue];
+            array.data[index+1] = colorMap.green[pxValue];
+            array.data[index+2] = colorMap.blue[pxValue];
+            array.data[index+3] = 0xff;
+            index += 4;
+        }
+        break;
+    
+    case "RGB":
+        // the planar configuration defines the memory layout
+        if( planarConfig !== 0 && planarConfig !== 1 ) {
+            throw new Error("Unsupported planar configuration: "+planarConfig);
+        }
+        sliceSize = image.getSize().getSliceSize();
+        sliceOffset = (sliceNumber || 0) * 3 * sliceSize;
+        // default: RGBRGBRGBRGB...
+        var posR = sliceOffset;
+        var posG = sliceOffset + 1;
+        var posB = sliceOffset + 2;
+        var stepPos = 3;
+        // RRRR...GGGG...BBBB...
+        if (planarConfig === 1) { 
+            posR = sliceOffset;
+            posG = sliceOffset + sliceSize;
+            posB = sliceOffset + 2 * sliceSize;
+            stepPos = 1;
+        }
+        
+        var redValue = 0;
+        var greenValue = 0;
+        var blueValue = 0;
+        for(var j=0; j < image.getSize().getSliceSize(); ++j)
+        {        
+            redValue = parseInt( windowLut.getValue( 
+                    image.getValueAtOffset(posR) ), 10 );
+            greenValue = parseInt( windowLut.getValue( 
+                    image.getValueAtOffset(posG) ), 10 );
+            blueValue = parseInt( windowLut.getValue( 
+                    image.getValueAtOffset(posB) ), 10 );
+            
+            array.data[index] = redValue;
+            array.data[index+1] = greenValue;
+            array.data[index+2] = blueValue;
+            array.data[index+3] = 0xff;
+            index += 4;
+            
+            posR += stepPos;
+            posG += stepPos;
+            posB += stepPos;
+        }
+        break;
+    
+    default: 
+        throw new Error("Unsupported photometric interpretation: "+photoInterpretation);
+    }
+};
+
+/**
+ * Add an event listener on the view.
+ * @method addEventListener
+ * @param {String} type The event type.
+ * @param {Object} listener The method associated with the provided event type.
+ */
+dwv.image.View.prototype.addEventListener = function(type, listener)
+{
+    var listeners = this.getListeners();
+    if( !listeners[type] ) {
+        listeners[type] = [];
+    }
+    listeners[type].push(listener);
+};
+
+/**
+ * Remove an event listener on the view.
+ * @method removeEventListener
+ * @param {String} type The event type.
+ * @param {Object} listener The method associated with the provided event type.
+ */
+dwv.image.View.prototype.removeEventListener = function(type, listener)
+{
+    var listeners = this.getListeners();
+    if( !listeners[type] ) {
+        return;
+    }
+    for(var i=0; i < listeners[type].length; ++i)
+    {   
+        if( listeners[type][i] === listener ) {
+            listeners[type].splice(i,1);
+        }
+    }
+};
+
+/**
+ * Fire an event: call all associated listeners.
+ * @method fireEvent
+ * @param {Object} event The event to fire.
+ */
+dwv.image.View.prototype.fireEvent = function(event)
+{
+    var listeners = this.getListeners();
+    if( !listeners[event.type] ) {
+        return;
+    }
+    for(var i=0; i < listeners[event.type].length; ++i)
+    {   
+        listeners[event.type][i](event);
+    }
+};
+
